fe883467c65e50c7d77c8e06b990fb17735a659a
paconovellino <pacognovellino@gmail.com>
2022-08-30 13:11:16 -0600

    cambios
    actualización de footer y de js para el boton

--- js/script.js
+++ js/script.js
@@ -6,10 +6,37 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
+
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
 }
+
 
 document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
@@ -31,32 +58,5 @@ var swiper = new Swiper(".home-slider", {
         clickable: true,
     },
     loop: true,
-});
-
-//   segundo slider
-
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    
-    loop: true,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        640: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 2,
-        },
-    },
 });
 
