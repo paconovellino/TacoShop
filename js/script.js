020e8f0474b0419128d1b9d7ec920063fa9a745e
paconovellino <pacognovellino@gmail.com>
2022-08-30 18:32:41 -0600

    actualizacion despues de clase
    arreglado el alt de las imagenes
    links en footer
    actualizado el js
    crear scss

--- js/script.js
+++ js/script.js
@@ -6,18 +6,31 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () => {
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar__bar a');
+
+window.onscroll = () =>{
+
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
-
-document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
-}
-
-document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.remove('active');
-}
+  
+    section.forEach(sec =>{
+  
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+  
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar__bar a[href*='+id+']').classList.add('active');
+        });
+      };
+  
+    });
+  
+  }
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
